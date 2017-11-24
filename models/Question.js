97bda85665aeac9757f17d693d96df9d691488b1
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-24 17:01:03 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -1,33 +1,28 @@
-const db = require('../config/db');
-
-class Question {
+const db = require("../config/db")
+ 
+class Question{
   static CreateTable() {
-    return new Promise(function(resolve) {
+    return new Promise(function(resolve){
       const sql = `CREATE TABLE questions (
         id INTEGER PRIMARY KEY,
         content TEXT
-      )`;
-
-      db.run(sql, function() {
-        resolve('questions table created');
-      });
-    });
+      )`
+ 
+      db.run(sql, function(){
+        resolve("questions table created")
+      })      
+    })
   }
-
+ 
   constructor(content){
     this.content = content
   }
-
+ 
   insert(){
-    const self = this // THIS IS THE CRUX
-    const sql = `INSERT INTO questions (content) VALUES (?)`
     return new Promise(function(resolve){
-      db.run(sql, [self.content], function(err, result){
-        self.id = this.lastID
-        resolve(self)      
-      })
+      resolve("This Does Nothing!")
     })
   }
 }
-
+ 
 module.exports = Question;
